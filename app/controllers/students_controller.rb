faa70c5b2de07157f3fd6fafca311620b4e84bc7
dlee16 <diana.lee116@gmail.com>
2019-03-15 18:06:48 -0400

    Teamwork

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,34 @@
+class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+  def new
+    @student = Student.new
+    render :new
+  end
+
+  def create
+    @student = Student.create(student_class_params)
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_class_params)
+    redirect_to student_path(@student.id)
+  end
+
+  private
+  def student_class_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+end
