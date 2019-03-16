9d7841b4f97a93d7f1a2b4a8b54852abc554d116
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-03-15 20:06:50 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# require 'faker'
+# #
+# #
+# # Student.destroy_all
+# students = 20.times do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
+#   Student.create({:first_name => "#{first_name}", :last_name => "#{last_name}"}) end
+
+# require 'faker'
+#
+#
+#
+# school_classes = 20.times do
+#   title = Faker::ProgrammingLanguage.name
+#   room_number = Faker::Number.number(3)
+#   SchoolClass.create({:title => "#{title}", :room_number => "#{room_number}"}) end
