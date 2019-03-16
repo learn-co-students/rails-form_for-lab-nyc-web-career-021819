bfb4e047b4113e53dafdc372051674ecbb1ac323
Kate Kim <katekim5809@gmail.com>
2019-03-16 01:16:21 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,38 @@
+class StudentsController < ApplicationController
+
+     def index
+      @students = Student.all
+    end
+
+     def show
+      @student = Student.find(params[:id])
+    end
+
+     def new
+      @student = Student.new
+    end
+
+     def edit
+      @student = Student.find(params[:id])
+    end
+
+
+     def create
+      @student = Student.create(params_permit)
+  	  redirect_to student_path(@student)
+  	end
+
+     def update
+      @student = Student.find(params[:id])
+      @student.update(params_permit)
+      redirect_to student_path(@student)
+    end
+
+     private
+  #strong params
+  #white list specific attributes
+    def params_permit
+      params.require(:student).permit(:first_name, :last_name)
+    end
+
+end
